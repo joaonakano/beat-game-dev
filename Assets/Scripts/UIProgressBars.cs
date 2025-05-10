ea05cd4e5f1f3a318c53efd5bb81d340419599a9
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-05-10 15:11:20 -0300

    tweak hud + code score fix

--- Assets/Scripts/UIProgressBars.cs
+++ Assets/Scripts/UIProgressBars.cs
@@ -4,16 +4,19 @@ using UnityEngine.UI;
 public class UIProgressBars : MonoBehaviour
 {
     [SerializeField] private Slider healthSlider;
-    [SerializeField] private Slider specialSlider;
+    [SerializeField] private Slider specialSlider1;
+    [SerializeField] private Slider specialSlider2;
 
     void Update()
     {
-        // Atualiza as barras com os valores do ScoreManager
         if (ScoreManager.Instance != null)
         {
-            // Atualiza valores entre 0 e 1 (ou 0 e 100 se preferir, depende da config do slider)
-            healthSlider.value = (float)ScoreManager.healthScore / 100f;
-            specialSlider.value = (float)ScoreManager.specialPercentageScore / 100f;
+            float healthValue = (float)ScoreManager.healthScore / 100f;
+            float specialValue = (float)ScoreManager.specialPercentageScore / 100f;
+
+            healthSlider.value = healthValue;
+            specialSlider1.value = specialValue;
+            specialSlider2.value = specialValue;
         }
     }
 }
