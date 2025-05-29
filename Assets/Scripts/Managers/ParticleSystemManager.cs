0de51f118fac33fdb6e4c71f1e10232e53ed302d
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-05-29 14:51:00 -0300

    Revert "alteracoes joao merge commit"
    This reverts commit 6b833e5fbd60fccee5f52ca7adeece463b6a4cff, reversing
    changes made to 240fff24c697b22713703292412a7002f5c4883c.

--- Assets/Scripts/Managers/ParticleSystemManager.cs
+++ Assets/Scripts/Managers/ParticleSystemManager.cs
@@ -5,6 +5,6 @@ public class ParticleSystemManager : MonoBehaviour
     public static void InstantiateHitParticles(ParticleSystem particleSystem, Vector3 coord, Quaternion rotation, float t)
     {
         ParticleSystem hitInstance = Instantiate(particleSystem, coord, rotation);
-        //Destroy(hitInstance.gameObject, t);
+        Destroy(hitInstance.gameObject, t);
     }
 }
--- Assets/Scripts/Menu/InGameMenuManager.cs
+++ Assets/Scripts/Menu/InGameMenuManager.cs
@@ -1,112 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class InGameMenuManager : MonoBehaviour
+public class GameMenuManager : MonoBehaviour
 {
-    [Header("Canva com o Menu In-Game")] 
-    public GameObject backgroundScreen;
+    public GameObject MainMenuGame;
+    public GameObject MenuOptions;
+    public GameObject MenuMusicSFX;
 
-    [SerializeField]
-    private bool isMenuBeingShown = false;
-
-    void Start()
-    {
-        if (InputManager.Instance == null)
-        {
-            Debug.LogError("InGameMenuManager: InputManager.Instance is null!");
-            return;
-        }
-
-        // Armazena no Evento do InputManager a função que exibe ou esconde o Menu
-        InputManager.Instance.OnMenuKeybindPressed += SetMenuState;
-    }
-
-    /// <summary>
-    /// Função que altera o estado do MENU: exibir ou esconder? Eis a questão.
-    /// </summary>
-    public void SetMenuState()
-    {
-        if (isMenuBeingShown)
-        {
-            HideMenu();
-            Debug.Log("HIDING THE MENU");
-        }
-        else
-        {
-            ShowMenu();
-            Debug.Log("SHOWING THE MENU");
-        }
-
-        isMenuBeingShown = !isMenuBeingShown;
-    }
-
-    /// <summary>
-    /// Método que esconde o Menu para o jogador e retoma a partida.
-    /// </summary>
-    public void HideMenu()
-    {
-        backgroundScreen.SetActive(false);
-        ResumeMatch();
-    }
-
-    /// <summary>
-    /// Método que exibe o Menu para o jogador e pausa a partida.
-    /// </summary>
-    public void ShowMenu()
-    {
-        backgroundScreen.SetActive(true);
-        SongManager.Instance.PauseSong();
-    }
-
-    /// <summary>
-    /// Método responsável por REINICIAR a partida.
-    /// Carrega a cena definida para o jogo.
-    /// </summary>
     public void RestartMatch()
     {
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
     }
 
-    /// <summary>
-    /// Método responsável por CONTINUAR a partida.
-    /// Define a música alguns TIMESAMPLES antes, assim a música volta alguns segundos e inicia novamente.
-    /// </summary>
     public void ResumeMatch()
     {
         int playbackRollbackInSeconds = SongManager.Instance.audioSource.timeSamples - 100000;
         SongManager.Instance.audioSource.timeSamples = playbackRollbackInSeconds;
         SongManager.Instance.UnpauseSong();
+        gameObject.SetActive(false);
     }
 
-    /// <summary>
-    /// Método responsável por voltar ao MENU PRINCIPAL a partir do MENU DO JOGO.
-    /// Carrega a Cena definida "MainMenu".
-    /// </summary>
     public void SwitchToMainMenu()
     {
         SceneManager.LoadSceneAsync("MainMenu");
     }
 
-    /// <summary>
-    /// Método responsável por entrar na aba CONFIGURAÇÕES dentro do Menu.
-    /// </summary>
+    public void BackToMenu()
+    {
+        MenuOptions.SetActive(false);
+        MainMenuGame.SetActive(true);
+    }
+
     public void GoToOptions()
     {
+        MainMenuGame.SetActive(false);
+        MenuOptions.SetActive(true);
     }
 
-    /// <summary>
-    /// Método responsável por.
-    /// </summary>
     public void GoToAudio()
     {
-    }
-
-    /// <summary>
-    /// Se esse InGameManager for desativado por algum motivo,
-    /// o Evento definido no InputManager é desvinculado deste Manager e fica vago para caso alguma outra função queira utilizá-lo.
-    /// </summary>
-    void OnDisable()
-    {
-        InputManager.Instance.OnMenuKeybindPressed -= SetMenuState;
+        MenuOptions.SetActive(false);
+        MenuMusicSFX.SetActive(true);
     }
 }
