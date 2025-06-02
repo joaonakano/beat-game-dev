6965c97772ea60e2bb34e5c2659a53202b06c3f8
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-06-02 11:03:22 -0300

    menu custom (WIP)

--- Assets/Scripts/Coisas do Emanuel/Menu/CameraMover.cs
+++ Assets/Scripts/Coisas do Emanuel/Menu/CameraMover.cs
@@ -1,42 +1,77 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class CameraMover : MonoBehaviour
 {
-    [Header("Posições da Câmera")]
-    public Transform mainMenuPosition;
-    public Transform settingsPosition;
-    public Transform levelsPosition;
-    public Transform creditsPosition;
+    [Header("Referências")]
+    public Transform mainMenuPos;
+    public Transform settingsPos;
+    public Transform levelsPos;
+    public Transform creditsPos;
 
-    [Header("Configurações de Animação")]
-    public float moveDuration = 1.0f;
-    public Ease easeType = Ease.InOutSine;
+    [Header("Fade")]
+    public Image fadeImage;
+    public float fadeDuration = 0.5f;
+
+    [Header("Movimento da Câmera")]
+    public float moveDuration = 1f;
+    public Ease moveEase = Ease.InOutSine;
 
     private Camera mainCamera;
 
-    private void Start()
+    void Start()
     {
         mainCamera = Camera.main;
+
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            fadeImage.color = new Color(0, 0, 0, 0); // Começa transparente
+        }
     }
 
     public void MoveToMainMenu()
     {
-        mainCamera.transform.DOMove(mainMenuPosition.position, moveDuration).SetEase(easeType);
+        StartCoroutine(FadeAndMove(mainMenuPos));
     }
 
     public void MoveToSettings()
     {
-        mainCamera.transform.DOMove(settingsPosition.position, moveDuration).SetEase(easeType);
+        StartCoroutine(FadeAndMove(settingsPos));
     }
 
     public void MoveToLevels()
     {
-        mainCamera.transform.DOMove(levelsPosition.position, moveDuration).SetEase(easeType);
+        StartCoroutine(FadeAndMove(levelsPos));
     }
 
     public void MoveToCredits()
     {
-        mainCamera.transform.DOMove(creditsPosition.position, moveDuration).SetEase(easeType);
+        StartCoroutine(FadeAndMove(creditsPos));
+    }
+
+    System.Collections.IEnumerator FadeAndMove(Transform target)
+    {
+        // Fade Out
+        yield return Fade(1);
+
+        // Movimenta a Câmera
+        mainCamera.transform.DOMove(target.position, moveDuration).SetEase(moveEase);
+        mainCamera.transform.DORotateQuaternion(target.rotation, moveDuration).SetEase(moveEase);
+
+        yield return new WaitForSeconds(moveDuration);
+
+        // Fade In
+        yield return Fade(0);
+    }
+
+    System.Collections.IEnumerator Fade(float targetAlpha)
+    {
+        if (fadeImage == null)
+            yield break;
+
+        fadeImage.DOFade(targetAlpha, fadeDuration);
+        yield return new WaitForSeconds(fadeDuration);
     }
 }
