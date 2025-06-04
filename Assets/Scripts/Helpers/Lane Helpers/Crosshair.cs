20e075451e4f98b1a4bbcf59c2d475cd3644f4b1
Joao Nakano <joaovynak@gmail.com>
2025-06-04 13:21:25 -0300

    commit crosshair rebind activation fix

--- Assets/Scripts/Helpers/Lane Helpers/Crosshair.cs
+++ Assets/Scripts/Helpers/Lane Helpers/Crosshair.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Crosshair : MonoBehaviour
 {
-    [SerializeField]
-    private KeyCode input;
+    [SerializeField] private string inputActionName;
 
     private Renderer crosshairRenderer;
-
     private float fadeLerpConstant = 8f;
+
+    private InputAction inputAction;
+
+    [SerializeField] private PlayerInput playerInput;
 
     void Start()
     {
         crosshairRenderer = GetComponent<Renderer>();
         crosshairRenderer.material.color = new Color(crosshairRenderer.material.color.r, crosshairRenderer.material.color.g, crosshairRenderer.material.color.b, 0.1f);
+
+        if (playerInput != null)
+        {
+            inputAction = playerInput.actions[inputActionName];
+        }
     }
 
     void Update()
     {
+        if (inputAction == null)
+            return;
+
         float newAlpha;
-        if (Input.GetKey(input))
+        if (inputAction.IsPressed())
         {
             newAlpha = Mathf.Lerp(crosshairRenderer.material.color.a, 1, fadeLerpConstant);
             crosshairRenderer.material.color = new Color(crosshairRenderer.material.color.r, crosshairRenderer.material.color.g, crosshairRenderer.material.color.b, newAlpha);
